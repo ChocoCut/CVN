229947bd02fc625dd5c4b6ebe3eeb7e9bcb418f4
Nick <nicolaslucero1997@gmail.com>
2015-08-13 13:20:14 -0700

    Implementing GameScene and using new AudioSystem.

--- src/tk/brytnah/cvn/scenes/MainMenu.java
+++ src/tk/brytnah/cvn/scenes/MainMenu.java
@@ -7,9 +7,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.media.AudioClip;
+import javafx.scene.media.MediaPlayer;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import javafx.util.Duration;
 import tk.brytnah.cvn.Main;
 
 /**
@@ -17,8 +18,10 @@ import tk.brytnah.cvn.Main;
 * @Date Aug 6, 2015
 * @Time 4:54:26 PM
 */
-public class MainMenu {
+public class MainMenu implements GameScene {
   final private Scene scene;
+  final private String sceneName = "MainMenu";
+  private MediaPlayer media;
   
   public MainMenu(){
     HBox btnBox;
@@ -51,13 +54,24 @@ public class MainMenu {
     
     group.setCenter(btnBox);
     buttons[3].setOnAction(e -> Main.pStage.close());
-    
-    AudioClip audio = (AudioClip) Main.audio.getAudioList().get("Overcast.mp3");
-    audio.setVolume(0.2);
-    audio.play();
   }
-  
+  @Override
   public Scene getScene(){
     return this.scene;
+  }
+  @Override
+  public String getName(){
+    return this.sceneName;
+  }
+  @Override
+  public final void startAudio(){
+    media = Main.audio.getMedia("Overcast.mp3");
+    media.setVolume(0.3);
+    media.setStopTime(new Duration(209897));
+    media.play();
+    media.setOnEndOfMedia(() -> {
+      media.seek(new Duration(2040));
+      media.play();
+    });
   }
 }
