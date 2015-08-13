256d79fe785c09e425a7e8a6c874b23b4319e736
Nick <nicolaslucero1997@gmail.com>
2015-08-13 12:33:51 -0700

    Reworked Audio System
    Allows more control over Audio files using MediaPlayer Class.

--- src/tk/brytnah/cvn/utility/Audio.java
+++ src/tk/brytnah/cvn/utility/Audio.java
@@ -4,6 +4,8 @@ package tk.brytnah.cvn.utility;
 import java.io.File;
 import java.util.HashMap;
 import javafx.scene.media.AudioClip;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 /**
 * @Author Nicolas
@@ -11,10 +13,23 @@ import javafx.scene.media.AudioClip;
 * @Time 5:01:09 PM
 */
 public class Audio {
-  final private HashMap<String, AudioClip> audioList = new HashMap<>();
   Resources resources = new Resources();
   
+  /**
+   * Even though it works its not
+   * entirely useful as it only
+   * plays an audio file and that's
+   * about it.
+   * 
+   * @deprecated
+   * 
+   * @author Nick
+   */
+  @Deprecated
+  final private HashMap<String, AudioClip> audioList = new HashMap<>();
+  @Deprecated
   public HashMap getAudioList() { return this.audioList; }
+  @Deprecated
   public boolean stopAudio(String audio){
     if(audioList.containsKey(audio)){
       audioList.get(audio).stop();
@@ -23,12 +38,48 @@ public class Audio {
       return false;
     }
   }
+  @Deprecated
   public boolean addAudio(String audio){
     File audioFile = new File(resources.getResource("audio/"+audio).getPath());
     if(audioFile.exists()){
       this.audioList.put(audio, new AudioClip(resources.getResource("audio/"+audio).toString()));
       return true;
     }else{
+      return false;
+    }
+  }
+  
+  /**
+   * New Audio System
+   * Uses MediaPlayer to allow seeking
+   * of certain times in audio file.
+   * 
+   * @author Nick
+   */
+  
+  final private HashMap<String, MediaPlayer> mediaList = new HashMap<>();
+  public MediaPlayer getMedia(String media){
+    if(mediaList.containsKey(media)){
+      return mediaList.get(media);
+    }else{
+      return null;
+    }
+  }
+  public boolean stopMedia(String media){
+    if(mediaList.containsKey(media)){
+      mediaList.get(media).stop();
+      return true;
+    }else{
+      return false;
+    }
+  }
+  public boolean addMedia(String media){
+    File mediaFile = new File(resources.getResource("audio/"+media).getPath());
+    if(mediaFile.exists()){
+      this.mediaList.put(media, new MediaPlayer(new Media(resources.getResource("audio/"+media).toExternalForm())));
+      return true;
+    }else{
+      System.out.println("Media file "+media+" does not exist.");
       return false;
     }
   }
